f8f9c114478d023695023e698b41541f70dcef22
Joaquin Bruno <joaquinbruno.93@gmail.com>
2023-01-30 14:00:40 -0300

    control pops implementado

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,10 +1,10 @@
 import { createContext, ReactElement } from 'react';
 
-
 import { useProduct } from '../hooks/useProduct';
-import { ProductContextProps, Product } from '../interfaces/interfaces';
+import { ProductContextProps, Product, onChangeArgs } from '../interfaces/interfaces';
 
 import styles from '../styles/styles.module.css'
+
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
@@ -14,12 +14,17 @@ export interface Props {
     children?: ReactElement | ReactElement[]
     className?: string;
     style?:  React.CSSProperties;
+    onChange?: (args: onChangeArgs) => void;
+    value?: number;
 }
 
-export const ProductCard = ({ children, product, className, style }: Props ) => {
+export const ProductCard = ({ children, product, className, style, onChange, value }: Props ) => {
 
-    const { counter, increaseBy } = useProduct();
+    const { counter, increaseBy } = useProduct({
+        onChange, product, value
+    });
 
+    
     return (
         <Provider value={{
             counter,
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,75 +1,79 @@
-import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
-import '../styles/custom-styles.css'
 
-const product = {
-    id: '1',
-    title: 'Coffee Mug - Card',
-    img: './coffee-mug.png'
-}
+import {
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+  ProductButtons,
+} from "../components";
+
+import "../styles/custom-styles.css";
+import { useShoppingcart } from "../hooks/useShoppingcart";
+import { products } from "../data/product";
+
+
 
 
 
 export const ShoppingPage = () => {
-    return (
-        <div>
-            <h1>Shopping Store</h1>
-            <hr />
 
-            <div style={{
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap'
-            }}>
-                {/*Propiedades propias del componente*/}
-             `<ProductCard 
-                product={product}
-                className="bg-dark text-white"
-                >
-                    <ProductCard.Image className="custom-image" />
-                    <ProductCard.Title className="text-white text-bold" />
-                    <ProductCard.Buttons
-                        className="custom-buttons"  />
-                </ProductCard>`
-                
-                {/*Construye las propiedades dentro del componente*/ }
-                <ProductCard 
-                    product={ product }
-                    className="bg-dark text-white"
-                    
-                >
-                    <ProductImage
-                        className="custom-image"
-                         />
-                    <ProductTitle
-                        className="text-white text-bold"
-                        activeClass="active"
-                    />
-                    <ProductButtons className="custom-buttons" 
-                    />
-                </ProductCard>
-                {/* Desde las props de cada Componente que tiene su propia prop, le paso dentro de la interface un style 
-                    Luego dentro de la function del componente le paso como argumento junto a las otras props la del style
-                */}
-                <ProductCard 
-                    product={ product }
-                    style={{
-                        backgroundColor: '#70D1F8'
-                    }}
-                    
-                >
-                    <ProductImage
-                    style={{boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}}
-                        />
-                    <ProductTitle style={{fontWeight: 'bold',}}
-                    />
-                    <ProductButtons
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'end',
-                        }}
-                    />
-                </ProductCard>
-            </div>
-        </div>
-    )
-}
+    const {onProductCountChange, shoppingCart} = useShoppingcart();
+
+  return (
+    <div>
+      <h1>Shopping Store</h1>
+      <hr />
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        {/*Propiedades propias del componente*/}
+
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className="bg-dark text-white"
+            onChange={ onProductCountChange }
+            value={shoppingCart[product.id]?.count || 0}
+            
+          >
+            <ProductImage className="custom-image" />
+            <ProductTitle
+              className="text-white text-bold"
+              activeClass="active"
+            />
+            <ProductButtons className="custom-buttons" />
+          </ProductCard>
+        ))}
+      </div>
+
+      <div className="shopping-cart">
+        {
+          Object.entries(shoppingCart).map( ([key, product ]) => (
+          <ProductCard
+          key={key}
+          product={product}
+          className="bg-dark text-white"
+          style={{width: '100px'}}
+          onChange={ onProductCountChange }
+          value={product.count}
+        >
+          <ProductImage className="custom-image" />
+          <ProductButtons className="custom-buttons" style={{display: 'flex', justifyContent: 'center' }} />
+        </ProductCard>
+          ))
+        }
+        
+
+      
+
+        
+      </div>
+
+    </div>
+  );
+};
